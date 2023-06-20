cdd5387ffc6cb0465bff91948aebaddcf6f1691a
고준서 <128276261+gjjss0903@users.noreply.github.com>
2023-06-20 12:08:09 +0900

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,12 @@
+$(function(){
+
+  $('.notice').click(function(){
+    $('.modal').addClass('active')
+  })
+  $('.btn-x').click(function(){
+    $('.modal').removeClass('active')
+  })
+  
+
+
+})
